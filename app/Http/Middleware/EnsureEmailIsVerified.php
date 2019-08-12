1dec8b41bb5d2c8de27a6b31a558457a0736d721
phpervip <2998658517@qq.com>
2019-08-12 10:00:25 +0000

    修改邮箱验证的条件

--- app/Http/Middleware/EnsureEmailIsVerified.php
+++ app/Http/Middleware/EnsureEmailIsVerified.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Models\User;
 
 class EnsureEmailIsVerified
 {
@@ -15,20 +16,24 @@ class EnsureEmailIsVerified
      */
     public function handle($request, Closure $next)
     {
-        // 三个判断：
-        // 1. 如果用户已经登录
-        // 2. 并且还未认证 Email
-        // 3. 并且访问的不是 email 验证相关 URL 或者退出的 URL。
+        // 四个判断：
+        // 1. 如果用户已经登录，
+        // 2. 邮箱不为空，
+        // 3. 并且还未认证 Email
+        // 4. 并且访问的不是 email 验证相关 URL 或者退出的 URL。
+
         if ($request->user() &&
-            ! $request->user()->hasVerifiedEmail() &&
+            $request->user()->email &&
+            ! $request->user()->hasVerifiedEmail()  &&
             ! $request->is('email/*', 'logout')) {
 
-            // 根据客户端返回对应的内容
-            return $request->expectsJson()
-                        ? abort(403, 'Your email address is not verified.')
-                        : redirect()->route('verification.notice');
-        }
+             // 根据客户端返回对应的内容
+                return $request->expectsJson()
+                            ? abort(403, 'Your email address is not verified.')
+                            : redirect()->route('verification.notice');
 
+
+        }
         return $next($request);
     }
 }

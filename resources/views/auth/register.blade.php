b4b0d3fe6584ae46ec8a44ba252ba9ab80a91e0c
phpervip <2998658517@qq.com>
2019-08-12 03:24:37 +0000

    注册验证码

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,23 @@
                             </div>
                         </div>
 
+                         <div class="form-group row">
+                            <label for="captcha" class="col-md-4 col-form-label text-md-right">验证码</label>
+
+                            <div class="col-md-6">
+                              <input id="captcha" class="form-control{{ $errors->has('captcha') ? ' is-invalid' : '' }}" name="captcha" required>
+
+                              <img class="thumbnail captcha mt-3 mb-2" src="{{ captcha_src('mini') }}" onclick="this.src='/captcha/mini?'+Math.random()" title="点击图片重新获取验证码">
+
+                              @if ($errors->has('captcha'))
+                                <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $errors->first('captcha') }}</strong>
+                                </span>
+                              @endif
+                            </div>
+                        </div>
+
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
